fe6bf014a8514b2a6a2b89abe2c068ab22573552
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2025-01-09 19:53:55 -0500

    created routes and initial structure

---
+++ app/testimonials/page.js
@@ -0,0 +1,14 @@
+export default function Testimonials() {
+    return (
+      <div>
+        <h1>Testimonials</h1>
+        <p>See what our customers are saying about us!</p>
+        <ul>
+          <li>"Great service!" - Customer A</li>
+          <li>"Highly recommend!" - Customer B</li>
+          <li>"Fantastic experience!" - Customer C</li>
+        </ul>
+      </div>
+    );
+  }
+  
