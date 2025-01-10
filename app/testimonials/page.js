95c34be9782df62cd9b95c4e3ee49d41e47ad017
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2025-01-09 20:00:19 -0500

    created routes and initial structure

--- app/testimonials/page.js
+++ app/testimonials/page.js
@@ -1,14 +1,17 @@
+import styles from "../page.module.css";
+
 export default function Testimonials() {
-    return (
-      <div>
-        <h1>Testimonials</h1>
-        <p>See what our customers are saying about us!</p>
-        <ul>
-          <li>"Great service!" - Customer A</li>
-          <li>"Highly recommend!" - Customer B</li>
-          <li>"Fantastic experience!" - Customer C</li>
-        </ul>
-      </div>
-    );
-  }
-  
+	return (
+		<div className={styles.page}>
+			<main className={styles.main}>
+				<h1>Testimonials</h1>
+				<p>See what our customers are saying about us!</p>
+				<ul>
+					<li>"Great service!" - Customer A</li>
+					<li>"Highly recommend!" - Customer B</li>
+					<li>"Fantastic experience!" - Customer C</li>
+				</ul>
+			</main>
+		</div>
+	);
+}
