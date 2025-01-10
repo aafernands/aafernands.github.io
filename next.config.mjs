f4e980a4c074a9675f11bbc8ad784cac5ee0a190
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2025-01-10 03:03:13 -0500

    Trigger build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Tells Next.js to export the project as static files
-  };
-  
-  export default nextConfig;
-  
+  output: 'export',
+};
+
+export default nextConfig;
