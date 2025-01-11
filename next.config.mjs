f12750ab944d3fd99fdf85db0888f84173d212a9
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2025-01-10 19:21:25 -0500

    Add latest changes2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: true, // Ensures each page has a trailing slash
-  };
-  
-  export default nextConfig;
-  
+  trailingSlash: true, // Ensures each page has a trailing slash
+  output: 'export', // Adds static export support
+};
+
+export default nextConfig;
