b83e12dff50927307a5b2d9c7bc9b13831b6cb83
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2025-01-10 23:06:29 -0500

    tidio chat implementation2

--- _document.js
+++ _document.js
@@ -1,26 +1,17 @@
-// pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          {/* Add any other meta tags or external resources here */}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-          
-          {/* Tidio Chat Script */}
-          <script
-            src="//code.tidio.co/sf7zqeagqkbig5d56iqlot3fq8nygg0k.js"
-            async
-          ></script>
-        </body>
-      </Html>
-    );
-  }
-}
+export default function MyDocument() {
+  return (
+    <Html>
+      <Head>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />  
+        
+            <script src="//code.tidio.co/sf7zqeagqkbig5d56iqlot3fq8nygg0k.js" async></script>
 
-export default MyDocument;
+      </body>
+    </Html>
+  );
+}
